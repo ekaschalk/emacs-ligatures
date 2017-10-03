6eef7cb3b3f5f81f7482973798599d4c82b94f95
ekaschalk <ekaschalk@gmail.com>
2017-10-03 16:08:28 -0700

    Add: minimal syntax propertize function composing region

--- lig-mode.el
+++ lig-mode.el
@@ -1,9 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.lig\\'" . lig-mode))
 
-(define-derived-mode lig-mode fundamental-mode "Lig"
-  (setq font-lock-defaults '(lig-font-lock-kwds))
-  (setq-local indent-line-function 'lisp-indent-line))
-
 (defconst lig--font-lock-kwds
   (list
    (rx "hello")
@@ -11,5 +7,29 @@
 
 (setq lig-font-lock-kwds
       (list lig--font-lock-kwds))
+
+(define-derived-mode lig-mode fundamental-mode "Lig"
+  (setq font-lock-defaults '(lig-font-lock-kwds))
+  (setq-local indent-line-function 'lisp-indent-line)
+  (setq-local syntax-propertize-function 'lig-syntax-propertize-function))
+
+(defun lig--match-lig (limit)
+  (re-search-forward
+   (rx word-start
+       "hello"
+       word-end)
+   limit
+   t))
+
+(defun lig-syntax-propertize-function (start end)
+  (save-excursion
+    (goto-char start)
+
+    (while (lig--match-lig end)
+      (compose-region (match-beginning 0)
+                      (match-end 0)
+                      #xe907)
+
+      )))
 
 (provide 'lig-mode)
