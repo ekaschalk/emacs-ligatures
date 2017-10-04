cf80374277765ef09417dde52c096f8c029530c7
ekaschalk <ekaschalk@gmail.com>
2017-10-03 23:33:42 -0700

    Add: further notes thoughts

--- lig-mode.el
+++ lig-mode.el
@@ -14,7 +14,10 @@
 (defun lig-mod-hook (overlay post-mod? start end &optional _)
   (when post-mod?
     (overlay-put overlay 'display nil)
-    (overlay-put overlay 'modification-hooks nil)))
+    (overlay-put overlay 'modification-hooks nil)
+    (set lig-overlay nil)))
+
+(setq lig-overlay nil)
 
 (defun lig-syntax-propertize-function (start-limit end-limit)
   (save-excursion
@@ -24,31 +27,63 @@
       (let ((start (match-beginning 0))
             (end (match-end 0)))
 
-        (setq lig-overlay (make-overlay start end))
-        (overlay-put lig-overlay 'display "")
-        (overlay-put lig-overlay 'modification-hooks '(lig-mod-hook))
+        (unless (-contains? (overlays-at start) lig-overlay)
+          (setq lig-overlay (make-overlay start end))
+          (overlay-put lig-overlay 'display "")
+          (overlay-put lig-overlay 'modification-hooks '(lig-mod-hook)))
 
+        (setq num-lines 1)
         (save-excursion
-          (forward-line)
-          (put-text-property (point) (+ 3 (point)) 'invisible t))))))
+          (while (> (lig-diff-in-indent start end num-lines) 0)
+            (forward-line num-lines)
+            (put-text-property (point) (+ 3 (point)) 'invisible t)
+            ;; (compose-region (point) (+ 4 (point)) ?\s)
+
+            (setq num-lines (1+ num-lines))
+          ))))))
+
+;; what if I maintained two separate buffers
+;; one with the composed indentation and the other with the true indentation
+;; the true buffer uses this overlay display trick
+;; the fake buffer uses compose region instead
+;; and we keep track of the indentation differences at each line
+;; and then use the spacing trick on all lines with different indentation
+
+;; "indirect buffers can have different major modes, overlays, markers"
+
+;; Create a blank mode that only takes indentation from parent mode
+;; create indirect buffer and set to this blank mode
+;; since it is an indirect buffer, they share text naturally
+
+;; since I cant make a separate overlay for compose
+;; I could instead make an invisible overlay mirroring the lig-overlay
+
+;; note can use evaporate property to nil the overlay automatically
+
+;; note look at after-string and before-string properties for overlay
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Overlay-Properties.html#Overlay-Properties
+
 
 (provide 'lig-mode)
 
-
-(defun lig-diff-in-indent (start end)
+(defun lig-diff-in-indent (start end num-lines)
   (compose-region start end "")
 
   (save-excursion
-    (forward-line)
-    (funcall (symbol-value 'indent-line-function))
-    (setq composed-indent (current-indentation)))
+    (dotimes (i num-lines)
+      (forward-line)
+      (funcall (symbol-value 'indent-line-function))
+      (setq composed-indent (current-indentation))))
 
   (decompose-region start end)
 
   (save-excursion
-    (forward-line)
-    (funcall (symbol-value 'indent-line-function))
-    (setq uncomposed-indent (current-indentation))))
+    (dotimes (i num-lines)
+      (forward-line)
+      (funcall (symbol-value 'indent-line-function))
+      (setq uncomposed-indent (current-indentation))))
+
+  (- uncomposed-indent composed-indent))
 
 
 ;;; Scratch
