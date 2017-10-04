a9993c498682ad40c4277436d2e814940305a26c
ekaschalk <ekaschalk@gmail.com>
2017-10-03 17:56:35 -0700

    Add: Able to distinguish indentation from compose/normal

--- lig-mode.el
+++ lig-mode.el
@@ -1,10 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
 ;; Notes
 ;; invisible text is ignored by indentation
-
-;; Thoughts
-;; what I make one space expand to many
-;;
-
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Specified-Space.html#Specified-Space
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Special-Properties.html#index-composition-_0040r_007b_0028text-property_0029_007d-3102
 
 ;;; Lig-mode
 
@@ -23,9 +22,6 @@
    limit
    t))
 
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Specified-Space.html#Specified-Space
-;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Special-Properties.html#index-composition-_0040r_007b_0028text-property_0029_007d-3102
-
 (defun lig-mod-hook (overlay post-mod? start end &optional x)
   (when post-mod?
     (overlay-put overlay 'display nil)
@@ -34,71 +30,42 @@
 
 (defun lig-syntax-propertize-function (start end)
   (save-excursion
-    ;; (goto-char (point-min))
-    (goto-char start)
+    ;; (goto-char start)
+    (goto-char (point-min))
 
     (while (lig--match-lig end)
-      ;; (compose-region (match-beginning 0) (match-end 0)
-      ;;                 #xe907)
-      ;; (put-text-property (match-beginning 0) (match-end 0)
-      ;;                    'display
-      ;;                    "")
+      (let ((start (match-beginning 0))
+            (end (match-end 0)))
 
-      ;; (put-text-property (match-beginning 0) (match-end 0)
-      ;;                    'invisible
-      ;;                    t)
+        ;; (setq lig-overlay
+        ;;       (make-overlay (match-beginning 0) (match-end 0)))
+        ;; (overlay-put lig-overlay 'display
+        ;;              "")
+        ;; (overlay-put lig-overlay 'modification-hooks
+        ;;              '(lig-mod-hook))
 
-      (setq lig-overlay
-            (make-overlay (match-beginning 0) (match-end 0)))
-      (overlay-put lig-overlay 'display "")
-      (overlay-put lig-overlay 'modification-hooks
-                   '(lig-mod-hook)
-                   )
+        (compose-region start end "")
 
-      ;; (overlay-put lig-overlay 'invisible t)
+        (save-excursion
+          (next-line)
+          (funcall (symbol-value 'indent-line-function))
+          (print (current-indentation))
+          )
 
-      ;; (put-text-property (match-beginning 0) (1- (match-end 0))
-      ;;                    'invisible
-      ;;                    t)
+        (decompose-region start end)
 
-      ;; (put-text-property (match-beginning 0) (match-end 0)
-      ;;                    'modification-hooks
-      ;;                    '(lig-mod-hook)
-      ;;                    )
-
-      ;; (save-excursion
-      ;;   (forward-line)
-      ;;   )
+        (save-excursion
+          (next-line)
+          (funcall (symbol-value 'indent-line-function))
+          (print (current-indentation))
+          )
+        )
       )))
 
 (provide 'lig-mode)
 
 ;;; Scratch
 ;;;; Scratch composition
-
-;; (compose-region (point) (+ 3 (point))
-;;                 ?\s)
-
-;; (unless (text-property-any (point) (1+ (point))
-;;                            'bigger t)
-
-;;   (compose-region (point) (+ 3 (point))
-;;                   ?\s))
-
-;; (put-text-property (point) (1+ (point))
-;;                    'bigger t)
-
-
-;;   (compose-region (point) (1+ (point))
-;;                   '(?\s (Br . Bl) ?\s))
-;;   )
-
-;; (put-text-property (point) (1+ (point))
-;;                    'bigger t)
-
-;; maybe I insert an invisible number of spaces on next line?
-;; what if I do the opposite - and shrink the spaces on next line
-;; and have the indentation adjust itself automatically?
 
 ;; `text-property-any'
 ;; `text-properties-at'
