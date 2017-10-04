b8d02ecad8eb6fc90a5808a3fb49261b12d54378
ekaschalk <ekaschalk@gmail.com>
2017-10-03 17:33:17 -0700

    Feature: Able to mimic compose region with overlays!

--- lig-mode.el
+++ lig-mode.el
@@ -1,13 +1,14 @@
+;; Notes
+;; invisible text is ignored by indentation
+
+;; Thoughts
+;; what I make one space expand to many
+;;
+
+
+;;; Lig-mode
+
 (add-to-list 'auto-mode-alist '("\\.lig\\'" . lig-mode))
-
-;; (defconst lig--font-lock-kwds
-;;   (list
-;;    (rx "hello")
-;;    '(0 font-lock-constant-face)
-;;    ))
-
-;; (setq lig-font-lock-kwds
-;;       (list lig--font-lock-kwds))
 
 (define-derived-mode lig-mode fundamental-mode "Lig"
   ;; (setq font-lock-defaults '(lig-font-lock-kwds))
@@ -22,37 +23,100 @@
    limit
    t))
 
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Specified-Space.html#Specified-Space
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Special-Properties.html#index-composition-_0040r_007b_0028text-property_0029_007d-3102
+
+(defun lig-mod-hook (overlay post-mod? start end &optional x)
+  (when post-mod?
+    (overlay-put overlay 'display nil)
+    (overlay-put overlay 'modification-hooks nil)
+    ))
+
 (defun lig-syntax-propertize-function (start end)
   (save-excursion
-    (goto-char (point-min))
+    ;; (goto-char (point-min))
+    (goto-char start)
 
     (while (lig--match-lig end)
-      (compose-region (match-beginning 0) (match-end 0)
-                      #xe907
-                      )
-      (save-excursion
-        (forward-line)
-        (unless (text-property-any (point) (1+ (point))
-                                   'bigger t)
+      ;; (compose-region (match-beginning 0) (match-end 0)
+      ;;                 #xe907)
+      ;; (put-text-property (match-beginning 0) (match-end 0)
+      ;;                    'display
+      ;;                    "")
 
-          (compose-region (point) (1+ (point))
-                          '(?\s (Br . Bl) ?\s))
-          )
+      ;; (put-text-property (match-beginning 0) (match-end 0)
+      ;;                    'invisible
+      ;;                    t)
 
-        (put-text-property (point) (1+ (point))
-                           'bigger t)
+      (setq lig-overlay
+            (make-overlay (match-beginning 0) (match-end 0)))
+      (overlay-put lig-overlay 'display "")
+      (overlay-put lig-overlay 'modification-hooks
+                   '(lig-mod-hook)
+                   )
 
-        ;; `text-property-any'
-        ;; `text-properties-at'
-        )
+      ;; (overlay-put lig-overlay 'invisible t)
 
-      ;; '(?\s (Br . Bl) ?\s)
-      ;; '(?\s (Br . Bl) ?\s
-      ;;       (Br . Bl) ?\s
-      ;;       (Br . Bl) ?\s
-      ;;       (Br . Bl) ?\s
-      ;;       (Br . Bl) ?\s)
-      ;; (prettify-utils-generate (" " "  "))
+      ;; (put-text-property (match-beginning 0) (1- (match-end 0))
+      ;;                    'invisible
+      ;;                    t)
+
+      ;; (put-text-property (match-beginning 0) (match-end 0)
+      ;;                    'modification-hooks
+      ;;                    '(lig-mod-hook)
+      ;;                    )
+
+      ;; (save-excursion
+      ;;   (forward-line)
+      ;;   )
       )))
 
 (provide 'lig-mode)
+
+;;; Scratch
+;;;; Scratch composition
+
+;; (compose-region (point) (+ 3 (point))
+;;                 ?\s)
+
+;; (unless (text-property-any (point) (1+ (point))
+;;                            'bigger t)
+
+;;   (compose-region (point) (+ 3 (point))
+;;                   ?\s))
+
+;; (put-text-property (point) (1+ (point))
+;;                    'bigger t)
+
+
+;;   (compose-region (point) (1+ (point))
+;;                   '(?\s (Br . Bl) ?\s))
+;;   )
+
+;; (put-text-property (point) (1+ (point))
+;;                    'bigger t)
+
+;; maybe I insert an invisible number of spaces on next line?
+;; what if I do the opposite - and shrink the spaces on next line
+;; and have the indentation adjust itself automatically?
+
+;; `text-property-any'
+;; `text-properties-at'
+;; '(?\s (Br . Bl) ?\s)
+;; '(?\s (Br . Bl) ?\s
+;;       (Br . Bl) ?\s
+;;       (Br . Bl) ?\s
+;;       (Br . Bl) ?\s
+;;       (Br . Bl) ?\s)
+;; (prettify-utils-generate (" " "  "))
+
+;;;; Scratch font lock
+
+;; (defconst lig--font-lock-kwds
+;;   (list
+;;    (rx "hello")
+;;    '(0 font-lock-constant-face)
+;;    ))
+
+;; (setq lig-font-lock-kwds
+;;       (list lig--font-lock-kwds))
